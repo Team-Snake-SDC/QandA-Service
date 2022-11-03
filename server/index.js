c9b6957c5f411a4e411e7c78f57ec6da5cb2f540
Parker Sturtevant <wsturtevant196@gmail.com>
2022-11-03 13:19:05 -0400

    successfully implement getQuestionsById

--- server/index.js
+++ server/index.js
@@ -1,21 +1,15 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const db = require('./queries');
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
+app.use(express.json());
 
 app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+	response.send({ info: 'Node.js, Express, and Postgres API' });
+});
 
-app.get('/questions', db.getQuestionsById)
+app.get('/qa/:product_id/questions', db.getQuestionsById);
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+	console.log(`App running on port ${port}.`);
+});
--- server/queries.js
+++ server/queries.js
@@ -1,27 +1,25 @@
-const Pool = require('pg').Pool
+const Pool = require('pg').Pool;
 const pool = new Pool({
-  user: 'parkersturtevant',
-  host: 'localhost',
-  database: 'Questions_and_Answers',
-  password: '',
-  port: 5432,
-})
-
+	user: 'parkersturtevant',
+	host: 'localhost',
+	database: 'Questions_and_Answers',
+	password: '',
+	port: 5432,
+});
 
 const getQuestionsById = (req, res) => {
-  query = {
-    text: `SELECT * FROM Question WHERE product_id = ${req.body.product_id} order by id limit ${req.body.count} offset ${(req.body.page * req.body.count) - req.body.count}`
-  }
-  pool.query(query.text, (error, data) => {
-    if (error) {
-      throw error
-    } else {
-      res.status(200).json(data.rows)
-    }
-  })
-}
-
+	const id = req.params.product_id || req.body.id;
+	pool
+		.query('SELECT * from questions where product_id = $1', [id])
+		.then((data) => {
+			res.send(data.rows);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).send(err);
+		});
+};
 
 module.exports = {
-  getQuestionsById
-}
+	getQuestionsById,
+};
