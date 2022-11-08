5fdf472463007c638e9c3656fc110980667bf2f9
Parker Sturtevant <wsturtevant196@gmail.com>
2022-11-08 14:34:26 -0500

    Updated env file

--- server/queries.js
+++ server/queries.js
@@ -1,10 +1,11 @@
 const Pool = require('pg').Pool;
+require('dotenv').config();
 const pool = new Pool({
-	user: 'parkersturtevant',
-	host: 'localhost',
-	database: 'Questions_and_Answers',
-	password: '',
-	port: 5432,
+	user: process.env.PGUSER,
+	host: process.env.PGHOST,
+	database: process.env.PGDATABASE,
+	password: process.env.PGPASSWORD,
+	port: process.env.PGPORT,
 });
 
 const getQuestionsById = (req, res) => {
